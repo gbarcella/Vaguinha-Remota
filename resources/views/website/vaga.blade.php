56b0307f4837c37a179dc8f404501579eceb4055
Gilto Barcella <barcellagilto@gmail.com>
2024-05-08 18:54:16 -0300

    finalizado primeira versao vaga especifica

--- resources/views/website/vaga.blade.php
+++ resources/views/website/vaga.blade.php
@@ -7,7 +7,7 @@
         <img src="{{$vaga->logotipo_empresa}}" alt="" class="img-card-vaga-especifica align-self-center">
         <div class="card-body">
             <h5 class="card-title">{{$vaga->titulo}}</h5>
-            <p class="card-text">Empresa: {{$vaga->nome_empresa}}</p>
+            <p class="card-text"><b>Empresa:</b> {{$vaga->nome_empresa}}</p>
             <p class="card-text"><b>Descrição:</b></p>
             <p class="card-text">{{$vaga->descricao}}</p>
             <hr>
@@ -26,6 +26,21 @@
                     <li class="list-group-item">{{$b}}</li>
                 @endforeach
             </ul>
+
+            <br>
+            <p class="card-text"><b>Remuneração</b></p>
+            <p class="card-text">{{$vaga->remuneracao}}</p>        
+
+            @if ($vaga->outras_informacoes)
+                <br>
+                <p class="card-text"><b>Outras Informações</b></p>
+                <p class="card-text">{{$vaga->outras_informacoes}}</p>            
+            @endif
+
+            <br>
+            <p class="card-text"><b>Publicação da Vaga</b></p>
+            <p class="card-text">{{$vaga->created_at->format('d/m/Y')}}</p>  
+            
 
             <br>
             <a href="{{$vaga->link_candidatura}}" target="_blank" class="btn btn-hero">Ir para candidatura</a>
