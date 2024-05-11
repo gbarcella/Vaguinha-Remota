91bf5a050f136a6f3e9270ed165097d3cd998ab3
Gilto Barcella <barcellagilto@gmail.com>
2024-05-11 13:25:25 -0300

    add adsense meta

--- resources/views/website/partials/base.blade.php
+++ resources/views/website/partials/base.blade.php
@@ -12,11 +12,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name', 'Laravel') }} - Encontre a sua vaga remota</title>
+    <meta name="google-adsense-account" content="ca-pub-7420756215098484">
 
-    {{-- AdSense Script --}}
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7420756215098484"
-     crossorigin="anonymous"></script>
+    <title>Vaguinha Remota - Encontre a sua vaga remota</title>
 
     {{-- Favicon --}}
     <link rel="icon" type="image/x-icon" href="{{ asset('img/site/favicon.png') }}">
