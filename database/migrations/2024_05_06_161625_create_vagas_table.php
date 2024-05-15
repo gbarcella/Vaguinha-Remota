86899fdf89056dc36e7d6dea596fbd345caa9bc0
Gilto Barcella <barcellagilto@gmail.com>
2024-05-15 17:52:39 -0300

    chang migrat

--- database/migrations/2024_05_06_161625_create_vagas_table.php
+++ database/migrations/2024_05_06_161625_create_vagas_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('vagas', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
-            $table->string('descricao');
-            $table->string('requisitos');
-            $table->string('beneficios');
-            $table->string('outras_informacoes');
+            $table->longText('descricao');
+            $table->longText('requisitos');
+            $table->longText('beneficios');
+            $table->longText('outras_informacoes');
             $table->string('link_candidatura');
             $table->string('nome_empresa');
             $table->string('logotipo_empresa')->default('https://images.tcdn.com.br/img/img_prod/840618/livre_teste_95_1_885fa8156198b5e059f20c92b095ecfc.jpg');
